ac1df3683d5fe1eb85efc70bf8ee689ff72a9530
Audio10 <claudiojesus_10@hotmail.com>
2020-08-15 21:51:13 -0500

    plugin

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,23 +1,44 @@
 // Define la funcion como un Objeto y recibe un objeto como config {}
 function MediaPlayer(config) {
-    this.media = config.el;
+  this.media = config.el;
+  this.plugins = config.plugins || [];
+
+  this._initPlugins();
+}
+
+MediaPlayer.prototype._initPlugins = function () {
+  this.plugins.forEach((plugin) => {
+    plugin.run(this);
+  });
+};
+
+// Cuando tomamos una function como clase, los metodos deben ser agregados al prototype
+MediaPlayer.prototype.play = function () {
+  this.media.play();
+};
+
+MediaPlayer.prototype.pause = function () {
+  this.media.pause();
+};
+
+MediaPlayer.prototype.togglePlay = function () {
+  if (this.media.paused) {
+    this.play();
+  } else {
+    this.pause();
   }
+};
 
-  // Cuando tomamos una function como clase, los metodos deben ser agregados al prototype
-  MediaPlayer.prototype.play = function() {
-    this.media.play();
-  };
+MediaPlayer.prototype.toggleMute = function () {
+  console.log('this is working'); 
+};
 
-  MediaPlayer.prototype.pause = function() {
-    this.media.pause();
-  };
+MediaPlayer.prototype.mute = function () {
+  this.media.muted = true;
+};
 
-  MediaPlayer.prototype.togglePlay = function() {
-    if (this.media.paused) {
-      this.play();
-    } else {
-      this.pause();
-    }
-  };
+MediaPlayer.prototype.unmute = function () {
+  this.media.muted = false;
+};
 
-  export default MediaPlayer;
+export default MediaPlayer;
--- assets/index.js
+++ assets/index.js
@@ -1,11 +1,14 @@
 import MediaPlayer from './MediaPlayer.js';
+import AutoPlay from './plugins/AutoPlay.js'
 
 const video = document.querySelector('video');
 
 // Construye el MediaPlayer con el objeto que pasa el el como propiedad
-const player = new MediaPlayer({ el: video });
+const player = new MediaPlayer({ el: video, plugins: [new AutoPlay] });
 
-const button = document.querySelector('button');
+const button = document.querySelector('#reproduce');
+const muteButton = document.getElementById('#mute')
 
 // Llama el metodo onclick con el togglePlay
 button.onclick = () => player.togglePlay();
+muteButton.onclick = () => player.toggleMute();
