1648ce0713b7d4ca599a1896419d1512d0c2fbba
Audio10 <claudiojesus_10@hotmail.com>
2020-08-27 23:15:59 -0500

    visibility_change

--- assets/plugins/AutoPause.js
+++ assets/plugins/AutoPause.js
@@ -3,6 +3,7 @@ class AutoPause {
     this.threshold = 0.25
     // Le dice que this es la instancia del objeto para handleIntersection es decir va a ser el plugin
     this.handleIntersection = this.handleIntersection.bind(this)
+    this.handleVisibilityChange = this.handleVisibilityChange.bind(this)
   }
 
   run(player) {
@@ -15,6 +16,8 @@ class AutoPause {
 
     // llama al metodo que observa y le envias que va a observar
     observer.observe(this.player.media)
+
+    document.addEventListener('visibilitychange', this.handleVisibilityChange)
   }
 
   // El metodo al ser ligado al observer, al llamar observe este envia una lista de entradas al metodo.
@@ -25,6 +28,16 @@ class AutoPause {
     // ********************************* THIS! en este caso seria el objeto observer porque es quien esta llamando a la funcion!!   *********************************************
     const isVisible = entry.intersectionRatio >= this.threshold
 
+    if (isVisible) {
+      this.player.play()
+    } else {
+      this.player.pause()
+    }
+  }
+
+  // Define a funtion that do the funtionality for the VisibilityChange
+  handleVisibilityChange() {
+    const isVisible = document.visibilityState === 'visible'
     if (isVisible) {
       this.player.play()
     } else {
