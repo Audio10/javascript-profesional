c5da7c41d361f13fc222388e38267a7aea9fd9d0
Audio10 <claudiojesus_10@hotmail.com>
2020-08-28 14:32:26 -0500

    service_workers

---
+++ sw.js
@@ -0,0 +1,45 @@
+/* eslint-disable no-restricted-globals */
+const VERSION = 'v1'
+
+async function precache() {
+  const cache = await caches.open(VERSION)
+  return cache.addAll([
+    '/',
+    '/index.html',
+    '/assets/index.js',
+    '/assets/MediaPlayer.js',
+    '/assets/plugins/AutoPlay.js',
+    '/assets/plugins/AutoPause.js',
+    '/assets/index.css',
+    '/assets/BigBuckBunny.mp4',
+  ])
+}
+
+async function cachedResponse(request) {
+  const cache = await caches.open(VERSION)
+  const response = await cache.match(request)
+  return response || fetch(request)
+}
+
+async function updateCache(request) {
+  const cache = await caches.open(VERSION)
+  const response = await cache.match(request)
+  return response || fetch(request)
+}
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(precache())
+})
+
+self.addEventListener('fetch', (event) => {
+  const { request } = event
+  // get
+  if (request.method !== 'GET') {
+    return
+  }
+
+  // buscar cache
+  event.respondWith(cachedResponse(request))
+
+  event.waitUntil(updateCache(request))
+})
